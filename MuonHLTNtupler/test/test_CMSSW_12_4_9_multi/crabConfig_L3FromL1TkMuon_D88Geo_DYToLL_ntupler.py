ff3d91878f8ae75c24a13b1c1888d4f50b3336c4
Sungwon Kim <kevinn9876@yonsei.ac.kr>
2022-10-12 17:38:05 +0200

    Including cmssw_12_4_9 hlt menus

---
+++ MuonHLTNtupler/test/test_CMSSW_12_4_9_multi/crabConfig_L3FromL1TkMuon_D88Geo_DYToLL_ntupler.py
@@ -0,0 +1,34 @@
+
+from CRABClient.UserUtilities import config, getUsernameFromCRIC
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+# config = config()
+
+config.section_('General')
+config.General.requestName = 'PU200-DYToLL_M-50_L3FromL1TkMuon_ntupler_20221010'
+config.General.workArea = 'crab_PhaseII_CMSSW_12_4_9_20221010'
+
+config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'Phase2_HLT_D88Geo_DYToLL_ntupler.py'
+config.JobType.maxMemoryMB = 4000
+config.JobType.maxJobRuntimeMin = 2750
+# config.JobType.inputFiles = ['L1TObjScaling.db']
+# config.JobType.outputFiles = ['ntuple.root', 'DQMIO.root']
+
+config.section_('Data')
+config.Data.allowNonValidInputDataset = True
+config.Data.inputDataset = '/DYToLL_M-50_TuneCP5_14TeV-pythia8/PhaseIISpring22DRMiniAOD-PU200_BSzpz35_123X_mcRun4_realistic_v11_ext1-v1/GEN-SIM-RECO'
+config.Data.inputDBS = 'global'
+# config.Data.splitting = 'Automatic'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 5
+config.Data.outLFNDirBase = '/store/user/sungwon/PhaseII_CMSSW_12_4_9/20221010/D88Geo_DYToLL_ntuple_for_Eff_check'
+config.Data.publication = False
+# config.Data.useParent = True
+# config.Data.ignoreLocality = True
+
+config.section_('Site')
+config.Site.storageSite = 'T3_KR_KNU'
+# config.Site.whitelist = ['T2_CH_CERN','T2_FR_*']
